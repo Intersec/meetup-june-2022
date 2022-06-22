8bcaf4f80741623956660eaef0b253dce48692c1
Nicolas Pauss <nicolas.pauss@intersec.com>
2022-06-22 17:23:15 +0200

    wordcount: parse wordcount arguments with lib-common parseopt().
    The wordcount executable takes a path to a file in arguments and will
    count the number of occurrences of each unique words in the file.
    
    Change-Id: I79f39def3bf73aae5ecc0b6160cf2946fc091cac

--- src/wordcount.c
+++ src/wordcount.c
@@ -17,9 +17,43 @@
 /***************************************************************************/
 
 #include <lib-common/core.h>
+#include <lib-common/parseopt.h>
+
+
+static const char *short_args_g = "<file_path>";
+
+static const char *long_usage_g[] = {
+    "Count the number of occurrences of each unique words in the file",
+    "located at <file_path>.",
+    NULL,
+};
+
+static struct {
+    bool opt_help;
+} wordcount_g;
+#define _G wordcount_g
+
+static popt_t opts_g[] = {
+    OPT_GROUP("Options:"),
+    OPT_FLAG('h', "help", &_G.opt_help, "show this help"),
+    OPT_END()
+};
+
 
 int main(int argc, char **argv)
 {
-    e_info("TODO wordcount");
+    const char *arg0 = NEXTARG(argc, argv);
+    const char *file_path;
+
+    /* Parse the arguments */
+    argc = parseopt(argc, argv, opts_g, 0);
+    if (argc != 1 || _G.opt_help) {
+        makeusage(_G.opt_help ? 0 : -1, arg0, short_args_g,
+                  long_usage_g, opts_g);
+    }
+
+    file_path = NEXTARG(argc, argv);
+    e_info("TODO wordcount on file `%s`", file_path);
+
     return 0;
 }
